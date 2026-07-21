30a25cc2caa55faf5c871b21849a380406ca4a6e
Tareq Alharbi <trooo8y2222@gmail.com>
2026-07-21 14:21:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+def calc_total(bill, tip_percent):
+    tip = bill * tip_percent / 100
+    return bill + tip
 
+def main():
+    bill = float(input("bill: "))
+    pct = float(input("tip %: "))
+    print(f"total = {calc_total(bill, pct)}")
+
+if __name__ == "__main__":
+    main()
